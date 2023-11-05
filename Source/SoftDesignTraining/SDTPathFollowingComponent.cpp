f9226d265d3787cf7043fc0a9d4c9cfe649ae06c
Dominik Courcelles <dominik.courcelles@gmail.com>
2023-11-05 11:55:26 -0500

    At the end of the jump curve, continue to reduce the height of the agent so Unreal detects that the agent is back on ground and triggers the change to the next path point.

--- Source/SoftDesignTraining/SDTPathFollowingComponent.cpp
+++ Source/SoftDesignTraining/SDTPathFollowingComponent.cpp
@@ -54,7 +54,7 @@ void USDTPathFollowingComponent::FollowPathSegment(float DeltaTime)
                 // This assumes that the starting and ending point are at the same Z coordinate.
                 // The detection of the character back on the floor is not properly detected and it prevents to move to the next segment. Add a small shift in the Z coordinate to help
                 // the detection of the floor.
-                newLocation = segmentStart.Location + FVector(JumpVector2D * (TimeOnCurve - MinTimeCurve) / (MaxTimeCurve - MinTimeCurve), owner->GetCharacter()->GetSimpleCollisionHalfHeight() - 5.0f);
+                newLocation = segmentStart.Location + FVector(JumpVector2D * (TimeOnCurve - MinTimeCurve) / (MaxTimeCurve - MinTimeCurve), owner->GetCharacter()->GetSimpleCollisionHalfHeight() - 20.0f * ((TimeOnCurve - MaxTimeCurve) / 0.2f));
             }
             else
             {
